8ff6eba85b1b442ac5e0a806546fb4f71692f54b
Syed Zain Anwer <zainanwer24@gmail.com>
2023-10-29 02:35:59 +0100

    added skeleton and playable devices

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,28 @@
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+
+interface propTypes {
+  game: Game;
+}
+
+const GameCard = ({ game }: propTypes) => {
+  return (
+    <Card width='220px' borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <Heading fontSize="xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacrtic} />
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,26 @@
 import useGames from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-    const {games, error} = useGames();
+  return (
+    <div>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {games && games.map((game) => <GameCard key={game.id} game={game} />)}
+      </SimpleGrid>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            {error && <Text>{error}</Text>}
-            <ul>
-                {games && games.map((game) => <li key={game.id}>{game.name}</li>)}
-            </ul>
-        </div>
-    )
-}
-
-export default GameGrid
+export default GameGrid;
